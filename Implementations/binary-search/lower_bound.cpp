b2649fda2f33c8a9a47372f27bbd41f10ea8eb65
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2023-12-01 14:23:14 -0300

    Update lower_bound.cpp

--- Implementations/binary-search/lower_bound.cpp
+++ Implementations/binary-search/lower_bound.cpp
@@ -1,1 +1,16 @@
+// encontra menor indice com valor >= x
+// caso x seja maior que todos os valores, sera retornado n
 
+void lwb(int x){
+	int l=-1; //l<x
+	int r=n;  //r>=x
+	while(r>l+1){
+		int m=(l+r)/2;
+		if(v[m]<x){
+			l=m;
+		} else {
+			r=m;
+		}
+	}
+	return r;
+}
