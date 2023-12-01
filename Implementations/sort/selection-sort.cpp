9f0d1a1099ac63e16789d0f9f1237882c36c6365
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2023-12-01 14:45:32 -0300

    Update selection-sort.cpp

--- Implementations/sort/selection-sort.cpp
+++ Implementations/sort/selection-sort.cpp
@@ -1,13 +1,13 @@
 //sorta em O(n^2)
-	
+
 	for(int i=0; i<n; i++){
-        menor=INF;
-        for(int j=0; j<n; j++){
-            if(marc[j]==0 && v[j]<menor){
-                menor=v[j];
-                idx=j;
-            }
-        }
-        cout << menor << " ";
-        marc[idx]=1;
-    }
+		menor=INF;
+		for(int j=0; j<n; j++){
+			if(marc[j]==0 && v[j]<menor){
+				menor=v[j];
+				idx=j;
+			}
+		}
+		cout << menor << " ";
+		marc[idx]=1;
+	}
