993eca17d94049463e9d0379e744a2dbd5b8bf68
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2024-01-04 15:55:38 -0300

    Create counting_sort.cpp

---
+++ Implementations/counting_sort.cpp
@@ -0,0 +1,10 @@
+//Time Complexity: O(n)
+
+	for(int i=0; i<n; i++){
+		freq[v[i]].push_back(v[i]);
+	}
+	for(int i=0; i<MAXN; i++){
+		if(freq[i].size()>0){
+			for(auto x : freq[i]) cout << x << " ";
+		}
+	}
