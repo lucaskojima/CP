12856a4d6a1cf2e6afc5fcf14d8e6b298a1b8e74
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2024-07-09 10:57:04 -0300

    Update template.cpp

--- Setup/template.cpp
+++ Setup/template.cpp
@@ -1,25 +1,21 @@
 #include "bits/stdc++.h"
-using namespace std;
- 
 #define ff first
 #define ss second
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
- 
-template<typename T>
-using matrix = vector<vector<T>>;
-using pii = pair<int, int>;
+
+using namespace std;
 using ll = long long;
- 
+using pii = pair<int, int>;
+
 const char nl = '\n';
-const ll LINF = (1LL<<60);
-const int INF = (1<<30);
- 
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int INF = 0x3f3f3f3f;
+
 int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	ios::sync_with_stdio(0), cin.tie(0);
 
-
+	
 	return 0;
 }
