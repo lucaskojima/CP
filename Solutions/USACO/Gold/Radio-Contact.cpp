bdfd478128600b5ca6b40fe50688296a6522065f
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2024-08-28 21:30:54 -0300

    Update Radio-Contact.cpp

--- Solutions/USACO/Gold/Radio-Contact.cpp
+++ Solutions/USACO/Gold/Radio-Contact.cpp
@@ -1,58 +1,73 @@
 #include "bits/stdc++.h"
-using namespace std;
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define int long long
 
-void setIO(string s){
-	ios::sync_with_stdio(0); cin.tie(0);
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+const char nl = '\n';
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int INF = 0x3f3f3f3f;
+
+void setIO(string s) {
+	ios::sync_with_stdio(0), cin.tie(0);
 	freopen((s+".in").c_str(),"r",stdin);
 	freopen((s+".out").c_str(),"w",stdout);
 }
 
-#define int long long
-#define ff first
-#define ss second
-template<typename T>
-using matrix = vector<vector<T>>;
-using pii = pair<int, int>;
-
-int32_t main(){
+int32_t main() {
 	setIO("radio");
 
 	int n, m; cin >> n >> m;
-	int fx, fy; cin >> fx >> fy;
-	int bx, by; cin >> bx >> by;
-	
-	vector<pii>f(n+1); f[0]={fx, fy};
-	for(int i=1; i<=n; i++){
-		char c; cin >> c;
-		if(c=='N') f[i]={f[i-1].ff, f[i-1].ss+1};
-		if(c=='E') f[i]={f[i-1].ff+1, f[i-1].ss};
-		if(c=='S') f[i]={f[i-1].ff, f[i-1].ss-1};
-		if(c=='W') f[i]={f[i-1].ff-1, f[i-1].ss};
+
+	vector<pii> fj(n + 1); cin >> fj[0].ff >> fj[0].ss;
+	vector<pii> vb(m + 1); cin >> vb[0].ff >> vb[0].ss;
+
+	string a, b; cin >> a >> b;
+
+	for (int i = 1; i <= n; i++) {
+		fj[i].ff = fj[i - 1].ff;
+		fj[i].ss = fj[i - 1].ss;
+
+		if (a[i - 1] == 'E') fj[i].ff++;
+		if (a[i - 1] == 'W') fj[i].ff--;
+		if (a[i - 1] == 'N') fj[i].ss++;
+		if (a[i - 1] == 'S') fj[i].ss--;
 	}
-	
-	vector<pii>b(m+1); b[0]={bx, by};
-	for(int i=1; i<=m; i++){
-		char c; cin >> c;
-		if(c=='N') b[i]={b[i-1].ff, b[i-1].ss+1};
-		if(c=='E') b[i]={b[i-1].ff+1, b[i-1].ss};
-		if(c=='S') b[i]={b[i-1].ff, b[i-1].ss-1};
-		if(c=='W') b[i]={b[i-1].ff-1, b[i-1].ss};
+
+	for (int i = 1; i <= m; i++) {
+		vb[i].ff = vb[i - 1].ff;
+		vb[i].ss = vb[i - 1].ss;
+
+		if (b[i - 1] == 'E') vb[i].ff++;
+		if (b[i - 1] == 'W') vb[i].ff--;
+		if (b[i - 1] == 'N') vb[i].ss++;
+		if (b[i - 1] == 'S') vb[i].ss--;
 	}
-	
-	auto dist=[&](pii a, pii b){
-		return (a.ff-b.ff)*(a.ff-b.ff)+(a.ss-b.ss)*(a.ss-b.ss);
+
+	vector<vector<int>> dp(n + 1, vector<int>(m + 1, INF));
+	dp[0][0] = 0;
+
+	auto sqr = [](int a) -> int {
+		return a * a;
 	};
-	
-	matrix<int>dp(n+1, vector<int>(m+1, 1e18)); dp[0][0]=0;
-	for(int i=0; i<=n; i++){
-		for(int j=0; j<=m; j++){
-			if(i==0 && j==0) continue;
-			if(i==0) dp[i][j]=dp[i][j-1]+dist(f[i], b[j]);
-			if(j==0) dp[i][j]=dp[i-1][j]+dist(f[i], b[j]);
-			if(i!=0 && j!=0) dp[i][j]=min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]})+dist(f[i], b[j]);
+
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= m; j++) {
+			if (i == 0 && j == 0) continue;
+
+			if (i != 0) dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+			if (j != 0) dp[i][j] = min(dp[i][j], dp[i][j - 1]);
+			if (i != 0 && j != 0) dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
+			dp[i][j] += sqr((fj[i].ff - vb[j].ff)) + sqr((fj[i].ss - vb[j].ss));
 		}
 	}
-	
-	cout << dp[n][m];
+
+	cout << dp[n][m] << nl;
 	return 0;
 }
