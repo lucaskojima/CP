acca40c85063be6d7cf2ca2d23f111e2a30b7616
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2024-12-18 15:01:54 -0300

    Update Almost-Union-Find.cpp

--- Solutions/UVa/Almost-Union-Find.cpp
+++ Solutions/UVa/Almost-Union-Find.cpp
@@ -1,72 +1,67 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
-const int MAXN=2e5+10;
-int n, m, pai[MAXN], prof[MAXN], sz[MAXN], sum[MAXN];
+const char nl = '\n';
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int INF = 0x3f3f3f3f;
 
-int find(int v){
-    if(pai[v]==v) return v;
-    return pai[v]=find(pai[v]);
+struct DSU {
+	vector<int> pai, siz, sum;
+	DSU(int n) : pai(2 * n + 1), siz(2 * n + 1), sum(2 * n + 1) {
+		for (int i = 1, j = n + 1; i <= n; i++, j++) {
+			pai[i] = j;
+			pai[j] = j;
+			siz[j] = 1;
+			sum[j] = i;
+		}
+	}
+	int find(int v) { return pai[v] == v ? v : pai[v] = find(pai[v]); }
+	void join(int a, int b) {
+		a = find(a), b = find(b);
+		if (a == b) return;
+		if (siz[a] > siz[b]) swap(a, b);
+		pai[a] = b;
+		siz[b] += siz[a];
+		sum[b] += sum[a];
+	}
+	void move(int a, int b) {
+		int repA = find(a), repB = find(b);
+		if (repA == repB) return;
+		siz[repA]--, sum[repA] -= a;
+		siz[repB]++, sum[repB] += a;
+		pai[a] = repB;
+	}
+};
+
+int main() {
+	ios::sync_with_stdio(0), cin.tie(0);
+
+	int n, m; while (cin >> n >> m) {
+		DSU dsu(n);
+		while (m--) {
+			int op; cin >> op;
+			if (op == 1) {
+				int p, q; cin >> p >> q;
+				dsu.join(p, q);
+			}
+			if (op == 2) {
+				int p, q; cin >> p >> q;
+				dsu.move(p, q);
+			}
+			if (op == 3) {
+				int p; cin >> p;
+				int rep = dsu.find(p);
+				cout << dsu.siz[rep] << ' ' << dsu.sum[rep] << nl;
+			}
+		}
+	}
+	return 0;
 }
-
-void une(int a, int b){
-    a=find(a); b=find(b);
-    if(a==b) return;
-    if(prof[a]>prof[b]){
-        pai[b]=a;
-        sz[a]+=sz[b];
-        sum[a]+=sum[b];
-        return;
-    }
-    pai[a]=b;
-    sz[b]+=sz[a];
-    sum[b]+=sum[a];
-    if(prof[a]==prof[b]) prof[b]++;
-}
-
-void move(int a, int b){
-    int k=a;
-    a=find(a); b=find(b);
-    sz[b]++; sum[b]+=k;
-    sz[a]--; sum[a]-=k;
-    pai[k]=b;
-}
-
-void ini(){
-    for(int i=1, j=n+1; i<=n; i++, j++){
-        pai[i]=j;
-        pai[j]=j;
-        sz[j]=1;
-        sum[j]=i;
-    }
-}
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    while(cin >> n >> m){
-        ini();
-        for(int i=0; i<m; i++){
-            int t; cin >> t;
-            if(t==1){
-                int p, q; cin >> p >> q;
-                une(p, q);
-            } else if(t==2){
-                int p, q; cin >> p >> q;
-                move(p, q);
-            } else if(t==3){
-                int p; cin >> p;
-                int f=find(p);
-                cout << sz[f] << " " << sum[f] << '\n';
-            }
-        }
-        
-        //clear
-        for(int i=1; i<=2*n; i++){
-            pai[i]=0; prof[i]=1; sz[i]=0; sum[i]=0;
-        }
-    }
-    return 0;
-}
-
