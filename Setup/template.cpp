23d1b5daa7fce6661ddd8949238f47419ff8d9b4
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2025-08-30 15:22:18 -0300

    Update template.cpp

--- Setup/template.cpp
+++ Setup/template.cpp
@@ -1,21 +1,18 @@
-#include <bits/stdc++.h>
-#define ff first
-#define ss second
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) (int)(x).size()
+#include "bits/stdc++.h"
+#define sz(x) (int)size(x)
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
 
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
 const char nl = '\n';
-const ll LINF = 0x3f3f3f3f3f3f3f3f;
 const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
 
-int main() {
-	ios::sync_with_stdio(0), cin.tie(0);
+int32_t main() {
+  ios::sync_with_stdio(0), cin.tie(0);
 
-	
-	return 0;
+  return 0;
 }
