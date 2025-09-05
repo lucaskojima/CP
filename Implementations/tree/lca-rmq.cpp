db091aa3aab699547e2a97dbc41a7d6c165af1bd
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2025-09-04 22:25:19 -0300

    Update lca-rmq.cpp

--- Implementations/tree/lca-rmq.cpp
+++ Implementations/tree/lca-rmq.cpp
@@ -1,7 +1,7 @@
 /**
  * Description: lowest common ancestor with rmq
  * Source: https://usaco.guide/gold/tree-euler?lang=cpp
- * Verification: https://judge.yosupo.jp/submission/312471
+ * Verification: https://judge.yosupo.jp/submission/312484
  * Time: O(1)
  */
 
@@ -10,15 +10,9 @@ struct lca_solver {
   vector<int> first;
   sparse_table<pii> st;
 
-  lca_solver(vector<vector<int>> &adj) : st(vector<pii>(1)) {
-    int n = sz(adj) - 1;
-    euler.reserve(2 *  n);
-    first.resize(n + 1);
+  lca_solver(const vector<vector<int>> &adj) : first(sz(adj)), st((dfs(adj, 1, -1), euler)) {}
 
-    dfs(adj, 1, -1);
-    st = sparse_table<pii>(euler);
-  }
-  void dfs(vector<vector<int>> &adj, int x, int p, int h = 0) {
+  void dfs(const vector<vector<int>> &adj, int x, int p, int h = 0) {
     first[x] = sz(euler);
     euler.push_back({h, x});
     for (int k : adj[x]) if (k != p) {
@@ -28,7 +22,7 @@ struct lca_solver {
   }
   int lca(int a, int b) {
     int l = first[a], r = first[b];
-    if (r < l) swap(l, r);
+    if (l > r) swap(l, r);
     return st.query(l, r).second;
   }
 };
