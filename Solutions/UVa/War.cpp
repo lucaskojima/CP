493c338ddcaa5f0b775d87d324676ead0fbf5143
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2026-05-18 20:41:30 -0300

    Update War.cpp

--- Solutions/UVa/War.cpp
+++ Solutions/UVa/War.cpp
@@ -1,67 +1,56 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
+
 using namespace std;
 
-const int MAXN=2e4+10;
-int n, pai[MAXN], prof[MAXN];
+struct dsu {
+  vector<int> e;
+  dsu(int n) : e(n, -1) {}
 
-int find(int v){
-    if(pai[v]==v) return v;
-    return pai[v]=find(pai[v]);
-}
+  int find(int v) {
+    return e[v] < 0 ? v : e[v] = find(e[v]);
+  }
+  bool same(int a, int b) {
+    return find(a) == find(b);
+  }
+  void join(int a, int b) {
+    a = find(a), b = find(b);
+    if (a == b) return;
+    if (e[a] < e[b]) swap(a, b);
+    e[b] += e[a];
+    e[a] = b;
+  }
+};
 
-void une(int a, int b){
-    a=find(a); b=find(b);
-    if(a==b) return;
-    if(prof[a]>prof[b]){
-        pai[b]=a;
-        return;
+int main() {
+  ios::sync_with_stdio(0), cin.tie(0);
+
+  int n; cin >> n;
+
+  dsu dsu(2 * n + 1);
+
+  auto F = [&](int a, int b) -> bool {
+    return dsu.same(a, b) && dsu.same(a + n, b + n);
+  };
+  auto E = [&](int a, int b) -> bool {
+    return dsu.same(a, b + n) && dsu.same(a + n, b);
+  };
+
+  int c, x, y;
+  while (cin >> c >> x >> y) {
+    if (c == 0) return 0;
+
+    if (c == 1) {
+      if (E(x, y)) { cout << -1 << '\n'; continue; }
+      dsu.join(x, y);
+      dsu.join(x + n, y + n);
+    } else if (c == 2) {
+      if (F(x, y)) { cout << -1 << '\n'; continue; }
+      dsu.join(x + n, y);
+      dsu.join(x, y + n);
+    } else if (c == 3) {
+      cout << (F(x, y) ? 1 : 0) << '\n';
+    } else {
+      cout << (E(x, y) ? 1 : 0) << '\n';
     }
-    pai[a]=b;
-    if(prof[a]==prof[b]) prof[b]++;
-}
-
-int enemy(int x){
-    return x+n;
-}
-
-bool is_friend(int a, int b){
-    if(find(a)==find(b) && find(enemy(a))==find(enemy(b))) return true;
-    return false;
-}
-
-bool is_enemy(int a, int b){
-    if(find(a)==find(enemy(b)) && find(enemy(a))==find(b)) return true;
-    return false;
-}
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> n;
-    for(int i=0; i<2*n; i++) pai[i]=i;
-    int c, x, y;
-    while(cin >> c >> x >> y){
-        if(c==0 && x==0 && y==0) break;
-        if(c==1){
-            if(is_enemy(x, y)){
-                cout << "-1\n";
-            } else {
-                une(x, y);
-                une(enemy(x), enemy(y));
-            }
-        } else if(c==2){
-            if(is_friend(x, y)){
-                cout << "-1\n";
-            } else {
-                une(x, enemy(y));
-                une(enemy(x), y);
-            }
-        } else if(c==3){
-            cout << (is_friend(x, y) ? "1\n" : "0\n");
-        } else if(c==4){
-            cout << (is_enemy(x, y) ? "1\n" : "0\n");
-        }
-    }
-    return 0;
+  }
 }
