fb268b91afbf95ffad43175afa7c28711381470c
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2026-07-03 14:47:07 -0300

    Update Taming-the-Herd.cpp

--- Solutions/USACO/Gold/Taming-the-Herd.cpp
+++ Solutions/USACO/Gold/Taming-the-Herd.cpp
@@ -8,7 +8,7 @@ template<class T> void chmin(T &a, T b) { if (b < a) a = b; }
 
 int main() {
   freopen("taming.in", "r", stdin);
-  freopen("taming.out", "w", stdout);
+	freopen("taming.out", "w", stdout);
   ios::sync_with_stdio(0), cin.tie(0);
 
   int n; cin >> n;
@@ -17,21 +17,26 @@ int main() {
   for (int i = 1; i <= n; i++)
     cin >> a[i];
 
-  vector dp(n + 1, vector(n + 1, vector<int>(n + 1, INF)));
-  vector dq(n + 1, vector<int>(n + 1, INF));
-  dp[0][0][0] = dq[0][0] = 0;
+  vector r(n + 1, vector<int>(n + 1));
+  for (int i = 1; i <= n; i++)
+    for (int j = i; j <= n; j++)
+      if (j == i)
+        r[i][j] = (a[i] == 0 ? 0 : 1);
+      else
+        r[i][j] = r[i][j - 1] + (a[j] == j - i ? 0 : 1);
+
+  vector dp(n + 1, vector<int>(n + 1, INF));
+  dp[0][0] = 0;
 
   for (int i = 1; i <= n; i++)
     for (int j = 1; j <= n; j++) {
-      dp[i][j][0] = dq[i - 1][j - 1] + (a[i] != 0);
-      for (int k = 1; k <= n; k++)
-        dp[i][j][k] = dp[i - 1][j][k - 1] + (a[i] != k);
-
-      for (int k = 0; k <= n; k++)
-        chmin(dq[i][j], dp[i][j][k]);
+      chmin(dp[i][j], dp[i - 1][j] + 1);
+      chmin(dp[i][j], dp[i - 1][j - 1] + 1);
+      if (i - a[i] - 1 >= 0)
+        chmin(dp[i][j], dp[i - a[i] - 1][j - 1] + r[i - a[i]][i]);
     }
 
-  for (int j = 1; j <= n; j++)
-    cout << dq[n][j] << '\n';
+  for (int i = 1; i <= n; i++)
+    cout << dp[n][i] << '\n';
   return 0;
 }
