14138fd57b4e6afd939c3672e1acb5544b4d3ee2
Lucas Kojima <121640073+lucaskojima@users.noreply.github.com>
2026-09-13 09:20:39 -0300

    Update template.cpp

--- Setup/template.cpp
+++ Setup/template.cpp
@@ -1,17 +1,9 @@
 #include "bits/stdc++.h"
-#define sz(x) (int)size(x)
-#define all(x) begin(x), end(x)
-#define rall(x) rbegin(x), rend(x)
+#define sz(x) int(size(x))
 
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
 
-const char nl = '\n';
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3f;
-
-int32_t main() {
+int main() {
   ios::sync_with_stdio(0), cin.tie(0);
 
   return 0;
